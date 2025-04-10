2d4721e76f1615e75652188b963572ad9cf20905
Saurabh Kumar <146050167+Saurabh-Kumar20-24@users.noreply.github.com>
2025-04-10 23:34:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,21 @@
 //your code here!
 
+
+function addItem(){
+	
+	for(let i=1; i<=20; i++){
+		let li=document.createElement("li");
+	li.textContent=`item${i}`;
+	document.getElementById("infi-list").appendChild(li);
+	}
+
+}
+
+addItem();
+
+let ol=document.getElementById("infi-list")
+	ol.addEventListener("scroll",()=>{
+	if(ol.scrollTop+ol.clientHeight>=ol.scrollHeight-5){
+		addItem();
+	}
+})
